818a9885a872c2f0565600adaaace77aa3719726
linomary <linomary@student.21-school.ru>
2025-04-28 13:30:22 +0700

    Task 15

---
+++ 15/client.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#define CHECK(item, msg, text, wrong) ({ if( item wrong) {\
+    msg(text);\
+    exit(EXIT_FAILURE);\
+    } })
+
+int main(int argc, char *argv[])
+{
+    int my_sock, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent* server;
+	char buffer[1024];
+    printf("TCP DEMO CLIENT\n");
+	
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    
+	my_sock = socket(AF_INET, SOCK_STREAM, 0);
+    CHECK(my_sock, perror, "socket", < 0);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    portno = atoi(argv[2]);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    
+    CHECK(connect(my_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)), perror, "connect", < 0);
+
+    while(1) {
+        n = recv(my_sock, &buffer[0], sizeof(buffer), 0);
+        CHECK(n, perror, "recieve", < 0);
+        buffer[n] = 0;
+        printf("S->%s", buffer);
+        printf("C->");
+        fgets(&buffer[0], sizeof(buffer) - 1, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+        send(my_sock, &buffer[0], strlen(&buffer[0]), 0);
+
+        if(strcmp(buffer, "5") == 0) {
+            printf("\nExit");
+            break;
+        }
+        printf("C1->");
+        fgets(&buffer[0], sizeof(buffer) - 1, stdin);
+        send(my_sock, &buffer[0], strlen(&buffer[0]), 0);
+        buffer[strcspn(buffer, "\n")] = 0;
+        
+        printf("C2->");
+        fgets(&buffer[0], sizeof(buffer) - 1, stdin);
+        send(my_sock, &buffer[0], strlen(&buffer[0]), 0);
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        n = recv(my_sock, &buffer[0], sizeof(buffer), 0);
+        CHECK(n, perror, "recieve", < 0);
+        buffer[strcspn(buffer, "\n")] = 0;
+        printf("S->%s\n", buffer);
+    }
+    close(my_sock);
+    return 0;
+
+}
---
+++ 15/server.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+
+
+
+#define CHECK(item, msg, text, wrong) ({ if( item wrong) {\
+    msg(text);\
+    exit(EXIT_FAILURE);\
+    } })
+
+int* nclients;
+
+void printusers();
+void handle(int);
+int sum(int, int);
+int sub(int, int);
+int mult(int, int);
+int divi(int, int);
+
+
+
+int main(int argc, char *argv[])
+{
+    char buff[1024];    // Буфер для различных нужд
+	int status;
+    printf("TCP SERVER DEMO\n");
+	
+    int shm_id = shm_open("/shmget", O_CREAT | O_RDWR, 0666);
+    CHECK(shm_id, perror, "get", == -1);
+    ftruncate(shm_id, sizeof(int));
+    
+    nclients = (int*) mmap(NULL, sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED, shm_id, 0);
+    CHECK(nclients, perror, "Map", == (int*)-1);
+
+	int sockfd, newsockfd;
+	int portno;
+	int pid;
+    socklen_t c_len;
+    struct sockaddr_in serv_addr, cli_addr;
+	
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    CHECK(sockfd, perror, "socket", < 0);
+	
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+	status = bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    CHECK(status, perror, "bind", < 0);
+
+	listen(sockfd,5);
+    c_len = sizeof(cli_addr);
+	
+	while (1)  {
+        newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr, &c_len);
+        CHECK(newsockfd, perror, "accept", < 0);
+	    *nclients+=1;
+        printusers();
+
+		
+        switch(pid = fork()) {
+            case -1:
+                perror("fork");
+                exit(EXIT_FAILURE);
+            case 0:
+            close(sockfd);
+            handle(newsockfd);
+            close(newsockfd);
+            *nclients =- 1;
+            exit(EXIT_SUCCESS);
+            default: 
+            close(newsockfd);
+        }
+     }
+     close(sockfd);
+     shutdown(sockfd, SHUT_RDWR);
+     close(shm_id);
+     shm_unlink("/shmget");
+     return 0;
+}
+
+void printusers() { 
+	if(*nclients)
+	{printf("%d user on-line\n",*nclients);}
+	else {printf("No User on line\n");}
+}
+int sum(int a, int b) {
+    return a+b;
+}
+int sub(int a, int b) {
+    return a - b;
+}
+int mult(int a, int b) {
+    return a*b;
+}
+int divi(int a, int b) {
+    if(a != 0) return a/b;
+    return 0;
+}
+
+void handle(int socket) {
+    char buffer[1024];
+    int recieved, a, b, cmd, result;
+    #define MSG_MSG "Command(1 - +, 2 - -, 3 - *, 4 - /)\n"
+    while(1) {
+        a = b = cmd = result = 0;
+        CHECK(write(socket, MSG_MSG, sizeof(MSG_MSG)), perror, "write msg", == -1);
+        printf("Open for input\n");
+
+        recieved = read(socket, &buffer[0], sizeof(buffer));
+        CHECK(recieved, perror, "read cmd", < 0);
+        cmd = atoi(buffer);
+        printf("Operation %d\n", cmd);
+        if(cmd < 5) {
+            printf("smd%d", cmd);
+            recieved = read(socket, &buffer[0], sizeof(buffer));
+            CHECK(recieved, perror, "read cmd", < 0);
+            a = atoi(buffer);
+            printf("First: %d\n", a);
+            recieved = read(socket, &buffer[0], sizeof(buffer));
+            CHECK(recieved, perror, "read cmd", < 0);
+            b = atoi(buffer);
+            printf("Second: %d\n", b);
+            switch(cmd) {
+                case 1:
+                    result = sum(a, b);
+                    break;
+                case 2:
+                    result = sub(a, b);
+                    break;
+                case 3:
+                    result = mult(a, b);
+                    break;
+                case 4:
+                    result = divi(a, b);
+                    break;
+            }
+            snprintf(buffer, strlen(buffer), "%d", result);
+            buffer[strlen(buffer)] = '\n';
+        } else if(cmd == 5) {
+            return;
+        }
+        else strcpy(buffer, "Error\n");
+        write(socket, &buffer[0], sizeof(buffer));
+    }
+
+}
